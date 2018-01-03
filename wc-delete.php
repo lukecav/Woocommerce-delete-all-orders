174ce7a267ad95cb3c1c014bb6c501694428c76a
Luke Cavanagh <lukecav@users.noreply.github.com>
2018-01-03 12:10:36 -0700

    Add plugin header
    More info added.

--- wc-delete.php
+++ wc-delete.php
@@ -1,12 +1,19 @@
 <?php
-/*
- * ----------------------------------------------------------------------------
- * "THE BEER-WARE LICENSE" (Revision 42):
- * <mauran@mauran.me> wrote this file.
- * As long as you retain this notice you can do whatever you want with this
- * stuff. If we meet some day, and you think this stuff is worth it, you can
- * buy me a beer in return --Mauran Muthiah
- * ----------------------------------------------------------------------------
+/**
+ * Plugin Name:          WooCommerce Delete All Orders
+ * Plugin URI:           https://github.com/lukecav/Woocommerce-delete-all-orders
+ * Description:          Delete all your Woocommerce orders with this script.
+ * Version:              1.0.0
+ * Author:               Luke Cavanagh
+ * Author URI:           https://github.com/lukecav
+ * License:              GPL2
+ * License URI:          https://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * WC requires at least: 3.0.0
+ * WC tested up to:      3.3.0-beta.1
+ *
+ * @package WooCommerce_Delete_All_Orders
+ * @author  Luke Cavanagh
  */
 
 // By default wp_ for standard WP-installations
